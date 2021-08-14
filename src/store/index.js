7e1b1ea7fe1ce6284cc80c2cda2d1ab7de13e948
Samuel <samkoj@hotmail.co.uk>
2021-08-14 16:47:55 +0100

    set up redux toolkit store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,28 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+	name: "counter", // every slice needs an id
+	initialState,
+	reducers: {
+		increment(state) {
+			// Here we are allowed to so called mutate state.
+			// Behind the scenes though, we aren't actually mutating state directly.
+			state.counter++;
+		},
+		decrement(state) {
+			state.counter--;
+		},
+		increase(state, action) {
+			state.counter = state.counter + action.amount;
+		},
+		toggleCounter(state) {
+			state.showCounter = !state.showCounter;
+		},
+	},
+});
 
 const counterReducer = (state = initialState, action) => {
 	/* 
