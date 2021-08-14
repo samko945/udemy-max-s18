89bc2b1ddbf2dfe5a524a51e21a9c74d60c4bb7c
Samuel <samkoj@hotmail.co.uk>
2021-08-14 09:46:17 +0100

    component modify state with actions using useDispatch hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,31 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+	// the useDispatch hook returns a dispatch function for actions.
+	const dispatch = useDispatch();
 	// useSelector automatically subscribes this component to the store, and will rerender on state change.
 	const counter = useSelector((state) => state.counter);
+
+	const incrementHandler = () => {
+		dispatch({ type: "increment" });
+	};
+
+	const decrementHandler = () => {
+		dispatch({ type: "decrement" });
+	};
+
 	const toggleCounterHandler = () => {};
 
 	return (
 		<main className={classes.counter}>
 			<h1>Redux Counter</h1>
 			<div className={classes.value}>{counter}</div>
+			<div>
+				<button onClick={incrementHandler}>Increment</button>
+				<button onClick={decrementHandler}>Decrement</button>
+			</div>
 			<button onClick={toggleCounterHandler}>Toggle Counter</button>
 		</main>
 	);
